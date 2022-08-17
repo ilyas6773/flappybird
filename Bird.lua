a43c5a891f91840718ae59a85ab720f43635f51e
ilyas6773 <mister.tolegenov@yandex.ru>
2022-08-17 15:41:19 +0600

    feat: added gravity variable and its corresponding mechanic into Bird class

--- Bird.lua
+++ Bird.lua
@@ -10,15 +10,28 @@
 
 Bird = Class{}
 
+local GRAVITY = 20
+
 function Bird:init()
     -- load bird image from disk and assign its width and height
     self.image = love.graphics.newImage('bird.png')
-    self.width = self.image.getWidth()
-    self.height = self.image.getHeight()
+    self.width = self.image:getWidth()
+    self.height = self.image:getHeight()
 
     -- position bird in the middle of the screen
     self.x = VIRTUAL_WIDTH / 2 - (self.width / 2)
     self.y = VIRTUAL_HEIGHT / 2 - (self.height / 2)
+
+    -- Y velocity(gravity)
+    self.dy = 0
+end
+
+function Bird:update(dt)
+    -- apply gravity to velocity
+    self.dy = self.dy + GRAVITY * dt
+
+    -- apply current velocity to Y position
+    self.y = self.y + self.dy
 end
 
 function Bird:render()
