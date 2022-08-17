0ff6af69a43c2866df1f00ebcdd479a612659dc9
ilyas6773 <mister.tolegenov@yandex.ru>
2022-08-17 14:47:52 +0600

    feat: made background and ground images infinitely scroll

--- main.lua
+++ main.lua
@@ -24,9 +24,24 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
+--
 -- images we load into memory from files to later draw onto the screen
+--
+
+-- background image and starting scroll location on X axis
 local background = love.graphics.newImage('background.png')
+local backgroundScroll = 0
+
+-- ground image and starting scroll location on X axis
 local ground = love.graphics.newImage('ground.png')
+local groundScroll = 0
+
+-- speed at which we should scroll our images, scaled by dt
+local BACKGROUND_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED = 60
+
+-- point at which we should loop our background back to X = 0
+local BACKGROUND_LOOPING_POINT = 413
 
 function love.load()
     -- initialize our nearest-neighbor filter
@@ -53,14 +68,28 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    -- scroll background by preset speed * dt, looping back to 0 after the looping point
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+
+    -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+end
+
 function love.draw()
     push:start()
     
-    -- draw the background starting at top left (0, 0)
-    love.graphics.draw(background, 0, 0)
+    -- here, we draw our images shifted to the left by their looping point; evetually,
+    -- they will revert back to 0 once a certain distance has elapsed, which will make it
+    -- seem as if they are infinitely scrolling. Choosing a looping point that is seameless
+    -- is key, so as to provide the illusion of looping
+
+    -- draw the background at the negative looping point
+    love.graphics.draw(background, -backgroundScroll, 0)
 
     -- draw the ground on top of the background, toward the bottom of the screen
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    -- at its negative looping point
+    love.graphics.draw(ground, -backgroundScroll, VIRTUAL_HEIGHT - 16)
     
     push:finish()
 end
