ecfa3683273e1a8f962245d6281586c6ee33c343
ilyas6773 <mister.tolegenov@yandex.ru>
2022-08-18 15:56:37 +0600

    ref, feat: added new class PipePair, allows to spawn not one pipe but two

--- main.lua
+++ main.lua
@@ -25,6 +25,9 @@ require 'Bird'
 -- pipe class we've written
 require 'Pipe'
 
+-- class representing pair of pipes together
+require 'PipePair'
+
 -- physical screen dimensions
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -56,10 +59,13 @@ local BACKGROUND_LOOPING_POINT = 413
 local bird = Bird()
 
 -- our table of spawning Pipes
-local pipes = {}
+local pipePairs = {}
 
 -- our timer for spawning pipes
 local spawnTimer = 0
+
+-- initialize our last recorder Y value for a gap placement to base other gaps off of
+local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
 function love.load()
     -- initialize our nearest-neighbor filter
@@ -113,10 +119,16 @@ function love.update(dt)
 
     spawnTimer = spawnTimer + dt
 
-    -- spawn a new Pipe if the timer is past 2 seconds
+    -- spawn a new PipePir if the timer is past 2 seconds
     if spawnTimer > 2 then
-        table.insert(pipes, Pipe())
-        print('Added new pipe!')
+        -- modify the last Y coordinate we placed so pipe gaps aren't too far apart
+        -- no higher than 10 pixels below the top edge of the screen,
+        -- and no lower than a gap length (90 pixels) from the bottom
+        local y = math.max(-PIPE_HEIGHT + 10, 
+            math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+        lastY = y
+
+        table.insert(pipePairs, PipePair(y))
         spawnTimer = 0
     end
     
@@ -124,12 +136,18 @@ function love.update(dt)
     bird:update(dt)
 
     -- for every pipe in the scene...
-    for k, pipe in pairs(pipes) do
-        pipe:update(dt)
+    for k, pair in pairs(pipePairs) do
+        pair:update(dt)
+    end
 
-        -- if pipe is no longer visible past left edge, remove it from scene
-        if pipe.x < -pipe.width then
-            table.remove(pipes, k)
+    -- remove any flagged pipes
+    -- we need this second loop, rather than deleting in the previous loop, because
+    -- modifying the table in-place without explicit key result in skipping the 
+    -- next pipe, since all implicit keys (numerical indices) are authomatically shifted
+    -- down after a table removal
+    for k, pair in pairs(pipePairs) do
+        if pair.remove then
+            table.remove(pipePairs, k)
         end
     end
 
@@ -149,8 +167,8 @@ function love.draw()
     love.graphics.draw(background, -backgroundScroll, 0)
 
     -- render all the pipes in our scene
-    for k, pipe in pairs(pipes) do
-        pipe:render()
+    for k, pair in pairs(pipePairs) do
+        pair:render()
     end
 
     -- draw the ground on top of the background, toward the bottom of the screen
