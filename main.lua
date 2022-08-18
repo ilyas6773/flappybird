3e8ce79f8113bf4063a19ea1521dd2ab6c2e53c5
ilyas6773 <mister.tolegenov@yandex.ru>
2022-08-18 21:57:22 +0600

    feat: added new Game State - Score Status, keeps track of the score and shows it and the end screen

--- main.lua
+++ main.lua
@@ -32,6 +32,7 @@ require 'PipePair'
 require 'StateMachine'
 require 'states/BaseState'
 require 'states/PlayState'
+require 'states/ScoreState'
 require 'states/TitleScreenState'
 
 -- physical screen dimensions
@@ -92,6 +93,7 @@ function love.load()
     gStateMachine = StateMachine {
         ['title'] = function() return TitleScreenState() end,
         ['play'] = function() return PlayState() end,
+        ['score'] = function() return ScoreState end
     }
     gStateMachine:change('title')
 
